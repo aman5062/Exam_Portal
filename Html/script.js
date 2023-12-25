b97ff611fc54d417a2fd3257a484be1f6bc459c7
jk825405 <154752042+jk825405@users.noreply.github.com>
2023-12-25 13:45:16 +0530

    Add files via upload

---
+++ Html/script.js
@@ -0,0 +1,23 @@
+/*const wrapper = document.querySelector('.warpper');
+const loginLink = document.querySelector('.Login-link');
+const registerlink = document.querySelector('regster-Link');
+
+const btnPopup= document.querySelector('btnLogin-popup');
+const iconClose = document.querySelector('.icon-Close');
+
+registerlink.addEventListener('click', ()=> {
+    wrapper.classList.add('active');
+});
+
+loginLink.addEventListener('click', ()=> {
+    wrapper.classList.remove('active');
+});
+
+btnPopup.addEventListener('click', ()=> {
+    wrapper.classList.add('active-Popup');
+});
+
+iconClose.addEventListener('click', ()=> {
+    wrapper.classList.add('active-Popup');
+}); */
+
